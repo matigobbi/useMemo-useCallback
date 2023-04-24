06385941242ebf99dd38f52789e5e029c3726d7f
matigobbi <matigobbi@gmail.com>
2023-04-24 10:53:44 +0200

    use ref implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
-import { useState } from 'react'
-import { useRef } from 'react'
+import { useState , useEffect , useRef} from 'react'
 import Movies from './components/movies'
 import useMovies from './hooks/useMovies'
 
 function App() {
   let apiKey = '6bedab68'
   const { movies } = useMovies()
-  const inputRef = useRef()
-
-  const handleClick=()=>{
-    
+  const [query, setQuery] = useState("")
+  const [error, setError] = useState(null)
+  let isFirstInput = useRef(true)
+  console.log(isFirstInput)
+  
+  const handleClick=(e)=>{
+     e.preventDefault()
+     console.log({query});
   }
+
+  const handleInputChange = (e) =>{
+    setQuery(e.target.value)
+
+
+  }
+
+  useEffect(()=>{
+    if(isFirstInput.current){
+      isFirstInput.current = query === ""
+      return
+    }
+
+    if(query === ""){
+      setError("POR FAVOR INTRODUZCA UNA BUSQUEDA")
+      return
+    }
+    if(query.lenght < 3){
+      setError("POR FAVOR INTRODUZCA MAS DE 3 CARACTERES")
+      return
+    }
+    setError(null)
+  },[query])
   return (
     <div className="page">
       <header>
@@ -18,12 +44,14 @@ function App() {
         <form className="form">
           <label> Search your movie</label>
           <input
-            ref={inputref}
+            onChange={handleInputChange}
             type="text"
             placeholder="Avengers, startwars, etc"
           />
-          <button onClick={handleClick} type="submit"></button>
+          <button onClick={handleClick} type="submit">Submit</button>
+          
         </form>
+          {error && <p style={{color:"red"}}>{error}</p>}
       </header>
       <main>
         <Movies movies={movies} />
